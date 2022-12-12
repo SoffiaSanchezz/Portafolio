39c208e2437eef5a838717002121fb9c997a83b1
Sofia Sanchez <ssofiiadayanna@gmail.com>
2022-12-12 18:10:15 -0500

    Progress in the portfolio

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../Pages/Portafolio'
+import Navbar from '../Componentes/Navbar'
 import imgone from '../assets/imgone.png'
 import ParticleBackground from '../Componentes/ParticlesBackground'
 import { NavLink } from 'react-router-dom'
@@ -14,8 +14,8 @@ return (
             <section className='sm:p-32 md:pb-0 p-10 flex'>
                 <div className='lg:w-1/2 w-full justify-center pt-12'>
                     <h2 className='text-7xl font-fredoka'>
-                        <div className='title font-BlackA'>¡Bienvenidos!</div>
-                        <div className='titleTwo font-Sniglet'>Conoce Mi Portafolio</div>
+                        <div className='font-BlackA text-violet-600 text-9sx1'>¡Bienvenidos!</div>
+                        <div className='text-white font-Sniglet'>Conoce Mi Portafolio</div>
                     </h2>
                     <div className='text-3xl py-6 md:h-28 h-28 font-rubik text-[#ffff]'></div>
                     <div className='pt-10 sm:pt-1 flex gap-3'>
