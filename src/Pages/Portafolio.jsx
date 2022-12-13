7047c93720a1f6a5e039176f97d9b9e01a6daefb
Sofia Sanchez <ssofiiadayanna@gmail.com>
2022-12-12 23:00:32 -0500

    Project Progress

--- src/Pages/Portafolio.jsx
+++ src/Pages/Portafolio.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import Cards from '../Componentes/Cards'
 import Navbar from '../Componentes/Navbar'
 import ParticleBackground from '../Componentes/ParticlesBackground'
+import Project from '../Componentes/Project'
 
 
 const Portafolio = () => {
@@ -14,7 +14,7 @@ const Portafolio = () => {
             <div className='flex w-full justify-center'>
                 <div className='flex flex-wrap gap-8 justify-center items-center pt-10 pb-6 sm:pt-36 w-11/12'>
                 
-                    <Cards/>
+                    <Project/>
                 </div>
             </div>
             <div className='pt-16'>
